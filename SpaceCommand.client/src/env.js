9a95da482e3093b0c9e7c898e71fead973a93261
Ryan-Thrall <ryanthrall04@gmail.com>
2023-06-28 12:10:58 -0600

    Setup Auth0

--- SpaceCommand.client/src/env.js
+++ SpaceCommand.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'ryanthrall.us.auth0.com'
+export const clientId = 'irbWjXnvszsGdJA18s8vuQcSYRjqx2ha'
+export const audience = 'https://codeworks-projects.com'
